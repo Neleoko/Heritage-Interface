1dd95e3ca7f64e8c6304a9aeb99295c97d4f0b96
neleoko <adda.ephraim@gmail.com>
2022-02-09 15:47:34 +0100

    Nouveau Projet

--- index.php
+++ index.php
@@ -6,22 +6,34 @@ include 'SportRelais.php';
 include 'SportBallons.php';
 include 'Club.php';
 
-echo "Projet Heritage et Interface<br>";
+// Init les clubs
+$club = array();
+$club[1] = new Club(1, "Dijon", 63);
+$club[2] = new Club(2, "PSG", 67);
+$club[3] = new Club(3, "Nantes", 65);
 
-$listClub[1] = new Club(1, "PSG", 234);
-$listClub[2] = new Club(2,"Lyon", 23);
-$listClub[3] = new Club(3,"Saint Etienne", 345);
-//--------------------------------------------------------------------------//
-$listClub[1]->AjouterSport(new SportRelais(nomSport: "4x100", nbJoueurs: 1, distance: 400));
-$listClub[1]->AjouterSport(new SportBallons(nomSport: "Basket", nbJoueurs: 8, longueur: 20, largeur: 30));
-$listClub[1]->AjouterSport(new Sport(nomSport: "football",nbJoueurs: 11));
+// Les sports de Dijon
+$sb = new SportBallon("Basketball", 5, 35, 45);
+$club[1]->AjouterSport($sb);
 
-$listClub[2]->AjouterSport(new SportRelais(nomSport: "100", nbJoueurs: 1, distance: 100));
-$listClub[2]->AjouterSport(new SportBallons(nomSport: "Handball", nbJoueurs: 8, longueur: 40, largeur: 50));
-$listClub[2]->AjouterSport(new Sport(nomSport: "VTT", nbJoueurs: 1));
+// Les sports de PSG
+$sport1 = new Sport("Javelot", 1);
+$sport2 = new Sport("Judo", 1);
+$sportBallon1 = new SportBallon("Football", 11, 68, 105);
+$sportBallon2 = new SportBallon("Handball", 7, 35, 65);
+$sportRelais1 = new SportRelais("4 * 100", 400, 4);
+$club[2]->AjouterSport($sport1);
+$club[2]->AjouterSport($sport2);
+$club[2]->AjouterSport($sportBallon1);
+$club[2]->AjouterSport($sportBallon2);
+$club[2]->AjouterSport($sportRelais1);
 
-$listClub[3]->AjouterSport(new SportRelais(nomSport: "200", nbJoueurs: 1, distance: 200));
-$listClub[2]->AjouterSport(new SportBallons(nomSport: "Pétanque", nbJoueurs: 6, longueur: 80, largeur: 90));
-$listClub[3]->AjouterSport(new Sport(nomSport: "Tennis", nbJoueurs: 2));
-
-
+// Les sports Nantes
+$sport3 = new Sport("Cyclisme", 1);
+$sportBallon3 = new SportBallon("FootBall", 11, 68, 105);
+$sportBallon4 = new SportBallon("Ruby", 15, 65, 115);
+$sportRelais2 = new SportRelais("100m", 100, 1);
+$club[3]->AjouterSport($sport3);
+$club[3]->AjouterSport($sportBallon3);
+$club[3]->AjouterSport($sportBallon4);
+$club[3]->AjouterSport($sportRelais2);
