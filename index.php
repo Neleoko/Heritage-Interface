a874662b757b9dd04ea2b58b22acad363a657b56
neleoko <adda.ephraim@gmail.com>
2022-02-14 17:28:59 +0100

    Nouveau Projet

--- index.php
+++ index.php
@@ -7,16 +7,36 @@ include 'SportBallon.php';
 include 'Club.php';
 include 'data.php';
 
-var_dump($_GET);
+
 
 
 foreach ($club as $keyClub => $valueClub){
-    echo '<a href="index.php?id='.$valueClub->getIdClub().'">' . $valueClub->getNomClub().'</a> <br>';
+    echo '<a href="result.php?id='.$valueClub->getIdClub().'">' . $valueClub->getNomClub().'</a> <br>';
 }
-if ($_GET["id"]){
-    echo "<br>Club" . $club[$_GET["id"]]->getNomClub()."<br>";
-    foreach ($club[$_GET["id"]]->getLesSports() as $keySport => $valueSport){
-        echo $valueSport->getNomSport()."<br>";
+if ($_GET["id"]) {
+    echo "<br>Club" . $club[$_GET["id"]]->getNomClub() . "<br>";
+    foreach ($club[$_GET["id"]]->getLesSports() as $keySport => $valueSport) {
+        echo $valueSport->getNomSport() . "<br>";
+    }
 }
+    echo "
+            <br>
+            Formulaire ID CLUB
+            </br>
+            <form method='post' name='formIdClub' action='result.php'>
+                <label for='pet-select'>Choose a pet: </label>
 
-}
+                <select name='pets' id='pet-select'>
+                    <option value=''>--Choisir un club--</option>";
+
+                foreach ($club as $kFormClub => $vFormClub)
+                {
+                    echo"<option value='".$vFormClub->getIdClub()."'>".$vFormClub->getNomClub()."</option>";
+                }
+                echo"</select>
+
+
+                <button type='submit'>Envoie</button>
+            </form>";
+
+
--- result.php
+++ result.php
@@ -1,7 +1,26 @@
 <?php
-if ($_GET["id"]) {
+include 'Comparable.php';
+include 'Isport.php';
+include 'sport.php';
+include 'SportRelais.php';
+include 'SportBallon.php';
+include 'Club.php';
+include 'data.php';
+
+if (isset($_GET["idClub"]) || isset($_POST["club"]) ) {
+    if (isset($_GET["idClub"])){
+        $idClub = $_GET["idClub"];
+    } else {
+        $idClub = $_POST["îdClub"];
+    }
     echo "<br>Club" . $club[$_GET["id"]]->getNomClub() . "<br>";
+
+    $listSport = $club[$idClub]->getLesSports();
+
     foreach ($club[$_GET["id"]]->getLesSports() as $keySport => $valueSport) {
-        echo $valueSport->getNomSport() . "<br>";
+        echo "-".$valueSport->getNomSport() . "<br>";
     }
 }
+
+var_dump($_POST);
+echo "<br> <a href='/index.php'>Retour</a>";
